c79128514edbe6b90b44a77944113ff02db7ed06
Кузьма Михайло <m.kuzma@trclux.com>
2017-08-22 23:49:40 +0300

    GITtest Bemrut frome home_PC

--- src/main/java/org/itstep/j2_16/dao/StudentDao.java
+++ src/main/java/org/itstep/j2_16/dao/StudentDao.java
@@ -2,7 +2,7 @@ package org.itstep.j2_16.dao;
 
 import java.util.List;
 import org.itstep.j2_16.entity.Student;
-
+// test Bemrut frome home PC
 public interface StudentDao {
 
     List<Student> getAll();
